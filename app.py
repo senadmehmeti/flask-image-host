76e3991a30422d1ed5a162e143171c8c75390bbd
senadmehmeti <senadmehmeti@proton.me>
2026-03-23 14:00:46 +0100

    Enhance PNG response and add log route
    Refactor response handling for PNG image and add logging route.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
 
@@ -13,7 +13,20 @@ def image():
         b'\x08\x06\x00\x00\x00\x1f\x15\xc4\x89\x00\x00\x00\nIDATx\x9cc\xf8\x0f'
         b'\x00\x01\x01\x01\x00\x18\xdd\x03\xdb\x00\x00\x00\x00IEND\xaeB`\x82'
     )
-    return png, 200, {"Content-Type": "image/png"}
+
+    resp = make_response(png)
+    resp.headers["Content-Type"] = "image/png"
+    resp.headers["Link"] = (
+        'https://flask-image-host.onrender.com/log; rel="preload"; as="image"; '
+        'referrerpolicy="unsafe-url"'
+    )
+    return resp
+
+@app.route("/log")
+def log():
+    ref = request.headers.get("Referer", "NO REFERER")
+    print("Referer:", ref)
+    return f"Referer: {ref}\n", 200, {"Content-Type": "text/plain"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
